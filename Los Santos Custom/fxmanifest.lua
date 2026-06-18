7b082ca978bc26f1d6cf7cfe3a2c068bed4cf98e
Franko <fugoku007@gmail.com>
2026-06-18 09:44:27 +0700

    LS custom

---
+++ Los Santos Custom/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_noyagarage_main.ytyp'
+
+Author 'FRANKO "NOYA STUDIO"'
+description 'Los Santos Custom Garage'
